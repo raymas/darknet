4add52028084f2acb19bc4cd9dd772a9c7f8b082
raymas <cSYvL6@yandex.com>
2019-03-16 20:42:29 +0100

    Updating interface

--- http/js/init.js
+++ http/js/init.js
@@ -1,7 +1,61 @@
 (function($){
   $(function(){
-
     $('.sidenav').sidenav();
-
   });
 })(jQuery);
+
+
+ALL_MESSAGES = {}
+
+
+function fetchData() {
+
+  $.getJSON('/data.json', function(data) {
+    for (var j=0; j < data.length; j++) {
+      var informations = data[j];
+      console.log(informations);
+    }
+  });
+}
+
+
+function createGraph(name, value, timestamp) {
+  var layout = {
+    title: name,
+    xaxis: {
+      title: 'Iterations',
+      showgrid: true,
+      zeroline: true
+    },
+    yaxis: {
+      title: '',
+      showline: false
+    },
+  };
+
+  var data = [{
+    x: [timestamp],
+    y: [value],
+    type: 'scatter'
+  }];
+
+  console.log(data);
+
+  ALL_MESSAGES[name] = data;
+
+  var mainContainer = document.getElementById('mainContent');
+
+  var newplotdiv = document.createElement('div');
+  newplotdiv.id = name;
+  newplotdiv.classList.add("control-panel");
+
+  mainContainer.appendChild(newplotdiv);
+
+  Plotly.newPlot(name, ALL_MESSAGES[name], layout, {responsive: true});
+}
+
+function updateGraph(name, value, timestamp) {
+  ALL_MESSAGES[name][0].x.push(timestamp);
+  ALL_MESSAGES[name][0].y.push(value);
+  Plotly.redraw(name);
+}
